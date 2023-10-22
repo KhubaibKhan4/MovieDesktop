ae9dcf947cfe595486e00d4739f0522bb6e082d9
Khubaib Khan <18.bscs.803@gmail.com>
2023-10-22 17:04:00 +0500

    Window State Added

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,29 @@
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.application
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.*
 import ui.MainScreen
 
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication, title = "Movie Desktop", icon = painterResource("logo.png")) {
+    val state = rememberWindowState(
+        placement = WindowPlacement.Floating,
+        isMinimized = true,
+        position = WindowPosition(alignment = Alignment.Center),
+        size = DpSize(width = 860.dp, height = 720.dp)
+    )
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Movie Desktop",
+        icon = painterResource("logo.png"),
+        visible = true,
+        state = state,
+        undecorated = false,
+        transparent = false,
+        resizable = true,
+        enabled = true,
+    ) {
         MainScreen()
     }
 }
